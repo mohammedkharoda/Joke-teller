a306c8651cd11eb04de0c1f9e88a0def66050622
mohammedkharoda <kharodawalam@gmail.com>
2022-05-21 12:24:00 +0530

    inital

---
+++ script.js
@@ -0,0 +1,47 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+import voiceRSS from "./voiceRSS";
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
+  // VoiceRSS Speech Parameters
+  voiceRSS.speech({
+    key: "93d20da9af8940d4996fa1fc7cba40a3",
+    src: jokeString,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    // Assign One or Two Part Joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+
+    tellMe(joke);
+
+    toggleButton();
+  } catch (error) {
+    console.log(`${error.message}☄️☄️☄️`);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
